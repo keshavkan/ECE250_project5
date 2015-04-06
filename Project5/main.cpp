b509f531334de836345aa55f3a785fabd7d69b3f
Keshav Kanatala <keskanatala@gmail.com>
2015-04-05 20:25:14 -0400

    Initial Commit

---
+++ Project5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project5
+//
+//  Created by Keshav Kanatala on 2015-04-05.
+//  Copyright (c) 2015 UWaterloo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
