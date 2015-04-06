6e2b2a1c855995ae99b0a290d1e8673ff5cd768a
Keshav Kanatala <keskanatala@gmail.com>
2015-04-06 19:58:45 -0400

    first draft

--- Project5/Weighted_graph.h
+++ Project5/Weighted_graph.h
@@ -85,17 +85,19 @@ bool Weighted_graph::comp_weight(const struct Edge &lhs, const struct Edge &rhs)
     return lhs.weight < rhs.weight;
 }
 
+
+
 /*********************************************************************
  * ***************************************************************** *
  * *                                                               * *
- * *   Constructor, Deconstructor				                   * *
+ * *   Constructor, Deconstructor, Operators	                   * *
  * *                                                               * *
  * ***************************************************************** *
  *********************************************************************/
 
 /*
- * Constructor: 
- *
+ * Constructor: Creates 2D adjacency matrix and degree array.
+ * 				Initializes them to 0 indicating no edges
  *
  */
 Weighted_graph::Weighted_graph( int n ) {
@@ -110,6 +112,8 @@ Weighted_graph::Weighted_graph( int n ) {
 	adjMatrix[0] = nullptr;
 	adjMatrix[1] = new double[adjSize];
 
+	//OPT delete dagArray
+	degArray[1] = degArray[0] = 0;
 
 	for (unsigned int i = 2; i < numV; i++) {
 		adjMatrix[i] = adjMatrix[i - 1] + i - 1;
@@ -119,16 +123,29 @@ Weighted_graph::Weighted_graph( int n ) {
 	for (unsigned int i = 0; i < adjSize; i++) {
 		 adjMatrix[1][i] = 0;
 	}
-
 }
 
 /*
- * Constructor: 
+ * Deconstructor: Deletes adjacency matrices and degree arrays
  *
  *
  */
 Weighted_graph::~Weighted_graph() {
+
+	//OPT delete degarray
+	delete [] degArray; 
+	delete [] adjMatrix[1];
+	delete [] adjMatrix;
 }
+
+
+std::ostream &operator<<( std::ostream &out, Weighted_graph const &graph ) {
+	// Your implementation
+
+	return out;
+}
+
+
 
 /*********************************************************************
  * ***************************************************************** *
@@ -139,9 +156,9 @@ Weighted_graph::~Weighted_graph() {
  *********************************************************************/
 
 /*
- * Constructor: 
+ * Accessor: int degree(int i)
  *
- *
+ * Returns: degree of vertex passed
  */
 int Weighted_graph::degree(int i) const {
 
@@ -152,30 +169,44 @@ int Weighted_graph::degree(int i) const {
     return degArray[i];
 }
 
+
 /*
- * Constructor: 
+ * Accessor: int edge_count()
  *
- *
+ * Returns: the number of edges in the graph by traversign through degArray
  */
 int Weighted_graph::edge_count() const {
 
 	unsigned int counter = 0;
 
-	//OPT LT array will be helpful
-	for (unsigned int i = 0; i < adjSize; i++) {
-		if (adjMatrix[1][i] != 0) {
-			counter++;
-		}
+	//OPT traversing through degArray will be faster sum(degArray)/2
+	// for (unsigned int i = 0; i < adjSize; i++) {
+	// 	if (adjMatrix[1][i] != 0) {
+	// 		counter++;
+	// 	}
+	// }
+
+	for (unsigned int i = 0; i < numV; i++) {
+		counter += degArray[i];
 	}
 
-	return counter;
+	return counter / 2;
 }
 
+
+/*
+ * Accessor: std::pair<double, int>  minimum_spanning_tree()
+ *
+ * Uses kruskal's algorith to calculate the minimum spanning tree. 
+ * Disjoint_sets data structure is needed
+ *
+ * Returns: a pair containing the weight of the graph and the number of edges scanned
+ */
 std::pair<double, int> Weighted_graph::minimum_spanning_tree() const {
 
 	unsigned int eg_count = 0;		//edge traversal count
 	double mst_weight = 0;			//tree weight
-	std::vector<Edge> eg_vector;	//Edge vector //OPT specify vector size
+	std::vector<Edge> eg_vector;	//Edge vector //OPT specify vector size(edge_count())
 
 	// traverse through adjMatrix and insert Edges to 
 	for (unsigned int i = 1; i < adjSize; i++) {
@@ -213,12 +244,6 @@ std::pair<double, int> Weighted_graph::minimum_spanning_tree() const {
 
 
 
-std::ostream &operator<<( std::ostream &out, Weighted_graph const &graph ) {
-	// Your implementation
-
-	return out;
-}
-
 /*********************************************************************
  * ***************************************************************** *
  * *                                                               * *
@@ -227,8 +252,81 @@ std::ostream &operator<<( std::ostream &out, Weighted_graph const &graph ) {
  * ***************************************************************** *
  *********************************************************************/
 
- bool Weighted_graph::insert_edge( int i, int j, double d ) {
-	return false;
+/*
+ * Mutator: bool insert_edge( int i, int j, double d )
+ *
+ * Returns all 
+ */
+bool Weighted_graph::insert_edge( int i, int j, double d ) {
+	
+	if (i >= numV || i < 0 || j >= numV || j < 0 || d <= 0) {
+		throw illegal_argument();
+	}
+
+	if (i == j) {
+		return false;
+	}
+
+	// assign larger vertex to i as adjMatrix is lower triangular
+	if (i < j) { std::swap(i,j); }
+
+	// insert edge weight to adjMatrix
+	adjMatrix[i][j] = d;
+
+
+	// OPT can remove degArray
+	degArray[i]++;
+	degArray[j]++;
+
+	return true;
+}
+
+
+/*
+ * Mutator: bool erase_edge( int i, int j )
+ *
+ * Returns true if and existing edge is erased. Else false
+ */
+bool Weighted_graph::erase_edge( int i, int j ) {
+
+	if (i >= numV || i < 0 || j >= numV || j < 0) {
+		throw illegal_argument();
+	}
+
+	if (i == j) {
+		return false;
+	}
+
+	if (i < j) { std::swap(i,j); }
+
+	// delete edge or return false
+	if (adjMatrix[i][j] == 0) {
+		return false;
+	} else {
+		adjMatrix[i][j] = 0;
+
+
+		// OPT can remove degArray
+		degArray[i]--;
+		degArray[j]--;
+
+
+		return true;
+	}
+}
+
+
+/*
+ * Mutator: void clear_edges()
+ *
+ * Clears all edges from the graph by resetting the adjMatrix.
+ */
+void Weighted_graph::clear_edges() {
+
+	// set adMatrix to 0, clearing all edges
+	for (unsigned int i = 0; i < adjSize; i++) {
+		 adjMatrix[1][i] = 0;
+	}
 }
 
 #endif
